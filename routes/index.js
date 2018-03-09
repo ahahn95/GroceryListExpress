9d8a9ee33cbfb27e4df61bd1a580aaa968471ca8
Alexander Hahn <ahahn95@gmail.com>
2018-03-09 13:33:49 -0500

    change homepage

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+const handleError = (res, reason, message, code) => {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('Home Page')
 });
 
 module.exports = router;
